2f05b92d8ee701bd3affbc547d0cbea99a5285f5
Rushil Kukreja <rushilkukreja@gmail.com>
2024-06-17 20:30:08 -0400

    Update rocket graphic
    Added additional rockets, changed layout, and revised data

--- vis/orbit_rocket.r
+++ vis/orbit_rocket.r
@@ -36,10 +36,15 @@ create_table <- function(starting_name, constellation) {
 create_table('aa_','Starlink')
 create_table('bb_','OneWeb')
 create_table('cc_','Kuiper')
+create_table('cc_','Guowang')
+create_table('cc_','Iridium')
+create_table('cc_','Globalstar')
+create_table('cc_','Orbcomm')
+create_table('cc_','Lightspeed')
 
 rct = read.csv(file.path(folder, '..', 'data', 'raw', "rockets_table.csv"),
                row.names = 1)
-new_names <- c('Falcon-9', 'Falcon-Heavy', 'Soyuz-FG', 'Ariane-5')
+new_names <- c('Falcon-9', 'Falcon-Heavy', 'Starship', 'Soyuz-FG', 'Ariane-5', 'Ariane-62')
 colnames(rct) <- new_names
 
 filename = 'dd_rockets_table.png'
@@ -49,9 +54,40 @@ dir.create(file.path(folder, 'figures'), showWarnings = FALSE)
 png(
   path,
   units = "in",
-  width = 12.5,
+  width = 15,
   height = 3,
   res = 480
 )
-print(grid.table(rct))
+table_grob <- tableGrob(rct)
+n_col <- ncol(rct)
+for (i in seq_len(n_col)) {
+  table_grob$widths[i] <- unit(0.13, "npc")
+}
+grid.newpage()
+grid.draw(table_grob)
+dev.off()
+
+rct = read.csv(file.path(folder, '..', 'data', 'raw', "rockets_table_2.csv"),
+               row.names = 1)
+new_names <- c('Ariane-64', 'LVM3', 'Vulcan Centaur', 'New Glenn', 'Long March-5', 'Atlas V')
+colnames(rct) <- new_names
+
+filename = 'dd_rockets_table_2.png'
+folder_tables = file.path(folder, 'figures', 'tables')
+path = file.path(folder_tables, filename)
+dir.create(file.path(folder, 'figures'), showWarnings = FALSE)
+png(
+  path,
+  units = "in",
+  width = 15,
+  height = 3,
+  res = 480
+)
+table_grob <- tableGrob(rct)
+n_col <- ncol(rct)
+for (i in seq_len(n_col)) {
+  table_grob$widths[i] <- unit(0.13, "npc")
+}
+grid.newpage()
+grid.draw(table_grob)
 dev.off()
